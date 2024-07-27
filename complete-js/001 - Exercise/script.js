8940412b6be12dc56badfbfd28925c3b7a5db55b
xaintobas <etsxaint@gmail.com>
2024-07-27 12:44:45 +0100

    JS P1 - Completed 4 Coding Challenge

--- complete-js/001 - Exercise/script.js
+++ complete-js/001 - Exercise/script.js
@@ -1,6 +1,5 @@
 ////////////////////////////////////
 // JavaScript Fundamentals – Part 1
-
 ////////////////////////////////////
 // Coding Challenge #1
 
@@ -65,8 +64,7 @@ if (markBMI1 > johnBMI1){
 ////////////////////////////////////
 // Coding Challenge #3
 
-/*
-There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+/* There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
 
 1. Calculate the average score for each team, using the test data below
 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
@@ -78,42 +76,41 @@ TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
 TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
-GOOD LUCK 😀
-*/
+GOOD LUCK 😀 */
 
-/*
-// const scoreDolphins = (96 + 108 + 89) / 3;
-// const scoreKoalas = (88 + 91 + 110) / 3;
-// console.log(scoreDolphins, scoreKoalas);
+console.log('');
+console.log('Coding Challenge #3');
 
-// if (scoreDolphins > scoreKoalas) {
-//   console.log('Dolphins win the trophy 🏆');
-// } else if (scoreKoalas > scoreDolphins) {
-//   console.log('Koalas win the trophy 🏆');
-// } else if (scoreDolphins === scoreKoalas) {
-//   console.log('Both win the trophy!');
-// }
+/* const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 106) / 3;
 
-// BONUS 1
-const scoreDolphins = (97 + 112 + 80) / 3;
-const scoreKoalas = (109 + 95 + 50) / 3;
-console.log(scoreDolphins, scoreKoalas);
+if (scoreDolphins > scoreKoalas) {
+  console.log(`Team Dolphins Wins 🏆 Scores: Dol ${scoreDolphins} - ${scoreKoalas} Koa `);
+} else if (scoreDolphins < scoreKoalas) {
+  console.log(`Team Koalas Wins 🏆 Scores: Dol ${scoreDolphins} - ${scoreKoalas} Koa`);
+} else {
+  console.log(`Draw for both teams. Dol ${scoreDolphins} - ${scoreKoalas} Koa`);
+} */
+
+// BONUS 
+
+const scoreDolphins = Math.floor((43 + 112 + 101) / 3);
+const scoreKoalas = Math.floor((109 + 95 + 106) / 3);
 
 if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-  console.log('Dolphins win the trophy 🏆');
-} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
-  console.log('Koalas win the trophy 🏆');
-} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
-  console.log('Both win the trophy!');
+  console.log(`Team Dolphins Wins 🏆 Scores: Dol ${scoreDolphins} - ${scoreKoalas} Koa `);
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  console.log(`Team Koalas Wins 🏆 Scores: Dol ${scoreDolphins} - ${scoreKoalas} Koa`);
+} else if((scoreDolphins === scoreKoalas) && scoreDolphins >= 100 && scoreKoalas >= 100) {
+  console.log(`Draw for both teams. Dol ${scoreDolphins} - ${scoreKoalas} Koa`);
 } else {
-  console.log('No one wins the trophy 😭');
+  console.log(`No one wins`);
 }
 
 ////////////////////////////////////
 // Coding Challenge #4
 
-/*
-Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+/* Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
 
 1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
 2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
@@ -123,15 +120,22 @@ TEST DATA: Test for bill values 275, 40 and 430
 HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
-GOOD LUCK 😀
-*/
+GOOD LUCK 😀 */
+
+console.log('');
+console.log('Coding Challenge #4');
+
+const bill = 40;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const total = bill + tip;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+
 
 /*
 const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
-
 
 ////////////////////////////////////
 // JavaScript Fundamentals – Part 2
