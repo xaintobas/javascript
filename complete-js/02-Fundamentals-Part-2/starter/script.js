e8dbb498fb22bb8537b0e4d7579938e6dfb60483
xaintobas <etsxaint@gmail.com>
2024-07-27 08:01:32 +0100

    Arrow Function

--- complete-js/02-Fundamentals-Part-2/starter/script.js
+++ complete-js/02-Fundamentals-Part-2/starter/script.js
@@ -51,6 +51,26 @@ const age2 = calcAge2(1996);
 console.log(age2);
 
 
+////////////////////////////////////
+// Arrow Function
+const calcAge3 = birthyear => 2024 - birthyear;
+const age3 = calcAge3(1996);
+console.log(age3);
 
+const yearsUntilRetirement = birthyear => {
+  const age = 2024 - birthyear;
+  const retirement = 65 - age;
+  return retirement;
+}
 
+console.log(yearsUntilRetirement(1995));
 
+const yearsUntilRetirement1 = (birthyear, firstName) => {
+  const age = 2024 - birthyear;
+  const retirement = 65 - age;
+  return `${firstName} reitres in ${retirement}`;
+}
+
+console.log(yearsUntilRetirement1(1996, 'Stephen'));
+
+console.log(yearsUntilRetirement1(2001, 'Helen'));
