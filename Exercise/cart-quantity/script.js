e0b52da0af4cbaf30605ad0a642ad0e2f5324140
xaintobas <etsxaint@gmail.com>
2024-07-30 03:06:39 +0100

    Updated the cart quantity exercise

--- Exercise/cart-quantity/script.js
+++ Exercise/cart-quantity/script.js
@@ -1,3 +1,31 @@
 let cartQuantity = 0;
 
-// document.getElementById('.btn-show').onclick(console.log('Cart is empty'));
+const showQuantity = function() {
+  console.log(`Cart quantity is: ${cartQuantity}`);
+  document.querySelector('.cartQuantityHtml').innerHTML = `${cartQuantity}`;
+}
+
+const addOne = function() {
+  cartQuantity += 1;
+  console.log(`Cart quantity is: ${cartQuantity}`); 
+  document.querySelector('.cartQuantityHtml').innerHTML = `${cartQuantity}`;
+}
+
+const addTwo = function() {
+  cartQuantity += 2;
+  console.log(`Cart quantity is: ${cartQuantity}`); 
+  document.querySelector('.cartQuantityHtml').innerHTML = `${cartQuantity}`;
+}
+
+const addThree = function() {
+  cartQuantity += 3;
+  console.log(`Cart quantity is: ${cartQuantity}`);
+  document.querySelector('.cartQuantityHtml').innerHTML = `${cartQuantity}`;
+}
+
+const reset = function() {
+  cartQuantity = 0;
+  console.log(`Cart was reset!!`); 
+  console.log(`Cart quantity is: ${cartQuantity}`); 
+  document.querySelector('.cartQuantityHtml').innerHTML = `${cartQuantity}`;
+}
