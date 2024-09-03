d69d8a60cb6c000d009461f4a652e15b7668adbe
xaintobas <etsxaint@gmail.com>
2024-09-03 17:24:54 +0100

    The for-of Loop

--- complete-js/09-Data-Structures-Operators/starter/script.js
+++ complete-js/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,62 @@
 'use strict';
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'St. Stephen Restaurant',
+  location: 'Benin-City, Edo State, Nigeria',
+  categories: ['Nigerian', 'African', 'Vegetarian'],
+  starterMenu: ['Akara', 'Moi Moi', 'Pepper Soup', 'Bole and Stew'],
+  mainMenu: ['Pounded Yam & Egusi Soup', 'Fufu & Ogbono', 'Jollof Rice'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    )
+  },
+
+  orderFriedRice: function(ing1, ing2, ing3) {
+    console.log(`Here is your deliciouse Fried Rice with ${ing1}, ${ing2}, and ${ing3}`);
+  }
+
+};
+
+///////////////////////////////////////
+// The for-of Loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()){
+  console.log(`${i + 1}: ${el}`);
+}
 
 
 ///////////////////////////////////////
@@ -70,7 +127,7 @@ const game = {
 let [players1, players2] = game.players;
 
 // 2. 
-[gk, ...fieldPlayers] = players1;
+let [gk, ...fieldPlayers] = players1;
 
 // 3.
 const allPlayers = [...players1, ...players2];
@@ -100,53 +157,7 @@ team1 > team2 && console.log('Team 2 might win');
 console.log(team1, draw, team2);
 
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'St. Stephen Restaurant',
-  location: 'Benin-City, Edo State, Nigeria',
-  categories: ['Nigerian', 'African', 'Vegetarian'],
-  starterMenu: ['Akara', 'Moi Moi', 'Pepper Soup', 'Bole and Stew'],
-  mainMenu: ['Pounded Yam & Egusi Soup', 'Fufu & Ogbono', 'Jollof Rice'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    )
-  },
-
-  orderFriedRice: function(ing1, ing2, ing3) {
-    console.log(`Here is your deliciouse Fried Rice with ${ing1}, ${ing2}, and ${ing3}`);
-  }
-
-};
 
 console.log('=======OBJECT DESTRUCTURING=========');
 // OBJECT DESTRUCTURING
