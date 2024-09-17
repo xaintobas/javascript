c4498ceb61e36f799a48066dbbe4193ec3ea7817
xaintobas <etsxaint@gmail.com>
2024-09-17 03:23:24 +0100

    09 Data Structures Operators

--- complete-js/09-Data-Structures-Operators/starter/script.js
+++ complete-js/09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
 
 ///////////////////////////////////////
 // Working With Strings - Part 1
@@ -649,16 +647,43 @@ Afterwards, test with your own test data!
 GOOD LUCK 😀
 */
 
+console.log('=======Coding Challenge #4=========');
+
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
   const textArea = document.querySelector('textarea').value;
-  console.log(textArea.split('_'));
+  const rows = textArea.split('\n');
+
+  for(const [i, row] of rows.entries()){
+    const trimWords = row.trim().toLowerCase();
+    const [firstWord, secondWord] = trimWords.split('_');
+    const output = `${firstWord}${secondWord.replace(secondWord[0], secondWord[0].toUpperCase())}`;
+    const padWord = output.padEnd(20);
+    const repeatedOk = '✅'.repeat(i + 1);
+    console.log(padWord, repeatedOk);
+  }
 });
 
 
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
 
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for(const flight of flights.split('+')){
+  const [type, from, to, time] = flight.split(';');
+
+  const output = `${type.startsWith('_Delayed') ? '🔴' : '' } ${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(46, '+');
+
+  console.log(output);
+}
 
