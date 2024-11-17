6cb55c016c68cc80d8ab7131e253b99a3bcc6024
xaintobas <etsxaint@gmail.com>
2024-11-17 11:19:36 +0100

    Node Js Tutorial class

---
+++ mosh-node/02-module-sys/logger.js
@@ -0,0 +1,19 @@
+// const url = "http://api.stephen.com/users";
+
+// function sayHello(name) {
+//   console.log(`Hello ${name}`);
+// }
+
+// module.exports = sayHello;
+
+// const EventEmitter = require("events");
+
+// class Logger extends EventEmitter {
+//   log(message) {
+//     console.log("Message");
+//     // RAISE AND EVENT
+//     emiter.emit("logging", { date: "Message Logged" });
+//   }
+// }
+
+// module.exports.log = Logger;
