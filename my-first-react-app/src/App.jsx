89001efbe02047225bb59443f0e607bcde4845c3
xaintobas <etsxaint@gmail.com>
2025-05-12 13:31:14 +0100

    my first react app

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+
+  return(
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ my-first-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+  return(
+    <footer>
+      <p>&copy; {new Date().getFullYear()} My First React Website</p>
+    </footer>
+  )
+}
+
+export default Footer;
