45d1da04efeccf8a8fdf6b94c0f53614dbbf4cb4
xaintobas <etsxaint@gmail.com>
2025-06-19 11:56:14 +0100

    UPDATED

--- complete-js/14-OOP/starter/script.js
+++ complete-js/14-OOP/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const stephen = new Person('Stephen', 1996);
+
+console.log(stephen);
+
+const Car = function (carName, speed) {
+  this.carName = carName;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const BMW = new Car('BMW', 120);
+const BENZ = new Car('BENZ', 95);
+
+BMW.accelerate();
